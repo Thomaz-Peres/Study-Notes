6a15e3c0f073e3edecfb984cbd5c9de19ceeea9c
Thomaz Peres <thomaz.peres21@hotmail.com>
2021-04-15 23:52:19 -0300

    Final de lista linear sequencial

---
+++ EstruturaDeDados/Aula3_&_Aula4_ListaLinear_&_ListaLinearSequencial/Aula4/ListaLinearSequencial2.c
@@ -0,0 +1,11 @@
+/** Na aula 3, 
+ * utilizamos um arranjo para armazenar os registros
+ * - E inserção de registros era feita na posição indica pelo usuario.
+**/
+
+/** Nesta aula (aula 4) - Nesta aula abordaremos dois aspectos
+ * - Otimização de busco por elementos
+ * Mudança na ordem de inserção dos elementos
+**/
+
+#insert <stdio.h>
