9c181f51e4882fbe491f16f476b8802e5250752b
Thomaz Peres <thomaz.peres21@hotmail.com>
2021-05-04 20:29:03 -0300

    Facul nao tava deixando eu estudar, Aula 9 pilha dinamica

---
+++ EstruturaDeDados/Aula9/Pilha_implementação_Dinamica.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef int TIPOCHAVE;
+
+typedef struct 
+{
+    TIPOCHAVE chave;
+
+}REGISTRO;
+
+
+typedef struct aux{
+    REGISTRO reg;
+    struct aux* prox;   //  a variavel prox, é um ponteiro para o elemento
+}ELEMENTO;
+
+typedef ELEMENTO* PONT;
+
+typedef struct{
+    PONT topo;
+}PILHA;
+
+
+//  Inicialização
+void inicializarPilha(PILHA* p)
+{
+    p->topo = NULL;
+}
+
+
+//  Retornar numero de elementos
+int tamanhoPilha(PILHA* p)
+{
+    PONT end = p->topo;
+    int tam = 0;
+
+    while(end != null)
+    {
+        tam++;
+        end = end->prox;
+    }
+    return tam;
+}
+
+
+//  Verificar se a pilha esta vazia
+bool pilhaEstaVazia(PILHA* p)
+{
+    if(p->topo == NULL) return true;
+    return false;
+}
+
+//  Exibição/Impressão
+int exibirPilha(PILHA* p)
+{
+    PONT end = p->topo;
+    
+    printf("Pilha: \" ");
+    while(end != null)
+    {
+        printf("%i ", end->reg.chave);
+        end = end->prox;
+    }
+    printf("\"\n");
+}
+
+
+
+//  Inserção de um elemento na pilha
+bool inserirElemPilha(PILHA* p, REGISTRO reg)
+{
+    PONT novo = (PONT) malloc(sizeof(ELEMENTO));
+    novo->reg = reg;
+    novo->prox = p->topo;
+    p->topo = novo;
+    return true;
+}
+
+
+//  Exclusão de um elemento
+bool excluirElemPilha(PILHA* p, REGISTRO reg)
+{
+    if(p->topo == NULL) return false;
+    *reg = p->topo->reg;
+    PONT apagar = p->topo;
+    p->topo = p->topo->prox;
+    free(apagar);
+    return true;
+}
+
+
+
+//  Reinicialização
+void reinicializarPilha(PILHA* p)
+{
+    PONT apagar;
+    PONT posicao = p->topo;
+
+    while(posicao != NULL)
+    {
+        apagar = posicao;
+        posicao - posicao->prox;
+        free(apagar);
+    }
+    p->topo = NULL;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ EstruturaDeDados/Aula9/Pilha_implementação_Dinamica.md
@@ -0,0 +1,18 @@
+# Pilha implementação Dinamica
+
+Na dinamica, é alocado e desalocado sobre demanda a memoria para os elementos
+
+- Vantagem: nao precisa desperdiçar memoria preparando de ante mão um arranjo.
+
+Porém é necessario arrumar uma forma de indicar como um elemento de relaciona a outro
+
+## Ideia
+![image](https://user-images.githubusercontent.com/58439854/117069175-923dde00-ad02-11eb-9b92-64608f45db9c.png)
+
+Como fariamos para adicionar um elemento ? 
+
+    - R:Por ser uma pilha, adicionara sempre no topo, então o numero adicionado aponta para o antigo topo, no caso o 5(2010 na memoria), e o topo aponta para a memoria do novo numero
+
+Como fariamos para excluir um elemento ? 
+
+    - R:Por ser uma pilha, excluira sempre o topo da lista, então o topo aponta para o proximo do antigo topo, e limpa o antigo da memoria
