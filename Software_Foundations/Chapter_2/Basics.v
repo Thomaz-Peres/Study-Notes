97161cab29aeb2c9e4c1e120cc100f9676c9d4d9
Thomaz <thomaz.peres21@hotmail.com>
2023-07-08 12:24:54 -0300

    Proof by simplification

--- Software_Foundations/Chapter_2/Basics.v
+++ Software_Foundations/Chapter_2/Basics.v
@@ -487,6 +487,7 @@ Fixpoint leb (n m : nat) : bool :=
       match m with
       | O => false
       | S m' => leb n' m'
+
       end
   end.
 
@@ -532,3 +533,96 @@ Example test_ltb2: (ltb 2 4) = true.
 Proof. simpl. reflexivity. Qed.
 Example test_ltb3: (ltb 4 2) = false.
 Proof. simpl. reflexivity. Qed.
+
+
+(* ------------------------------------------------------------------------ *)
+(* Proof by Simplification *)
+(* Now that we've defined a few datatypes and functions, let's turn to stating and proving properties of their behavior.
+Actually, we've already started doing this: each Example in the previous sections makes a precise claim about the
+behavior of some function on some particular inputs. The proofs of these claims were always the same: use simpl
+to simplify both sides of the equation, then use reflexivity to check that both sides contain identical values. *)
+
+(* The same sort of "proof by simplification" can be used to prove more interesting properties as well.
+For example, the fact that 0 is a "neutral element" for + on the left can be proved just by observing
+that 0 + n reduces to n no matter what n is -- a fact that can be read directly off the definition of plus. *)
+(* the universal quantifier ∀ using the reserved identifier "forall." *)
+Theorem plus_O_n : forall n : nat, 0 + n = n.
+Proof.
+  intros n. simpl. reflexivity. Qed.
+
+(* 
+  This is a good place to mention that [reflexivity] is a bit more
+  powerful than we have acknowledged. In the examples we have seen,
+  the calls to [simpl] were actually not needed, because
+  [reflexivity] can perform some simplification automatically when
+  checking that two sides are equal; [simpl] was just added so that
+  we could see the intermediate state -- after simplification but
+  before finishing the proof.  Here is a shorter proof of the
+  theorem:
+*)
+Theorem plus_O_n' : forall n : nat, 0 + n = n.
+Proof.
+intros n. reflexivity. Qed.
+(* 
+Moreover, it will be useful to know that reflexivity does somewhat
+more simplification than simpl does - for example,
+    it tries "unfolding" defined terms, replacing them with their
+    right-hand sides.  The reason for this difference is that, if
+    reflexivity succeeds, the whole goal is finished and we don't need
+    to look at whatever expanded expressions [reflexivity] has created
+    by all this simplification and unfolding;
+    by contrast, [simpl] is used in situations where we may have to
+    read and understand the new goal that it creates, so we would not
+    want it blindly expanding definitions and leaving the goal in a messy state
+    
+    First, we've used the keyword [Theorem] instead of [Example].
+    This difference is mostly a matter of style; the keywords
+    [Example] and [Theorem] (and a few others, including [Lemma],
+    [Fact], and [Remark]) mean pretty much the same thing to Coq.
+
+    Second, we've added the quantifier [forall n:nat], so that our
+    theorem talks about _all_ natural numbers [n].  Informally, to
+    prove theorems of this form, we generally start by saying "Suppose
+    [n] is some number..."  Formally, this is achieved in the proof by
+    [intros n], which moves [n] from the quantifier in the goal to a
+    _context_ of current assumptions. Note that we could have used
+    another identifier instead of [n] in the [intros] clause, (though
+    of course this might be confusing to human readers of the proof):
+*)
+
+Theorem plus_O_n'' : forall n : nat, 0 + n = n.
+Proof.
+  intros m. reflexivity. Qed.
+
+(* ------------------------------------------------------------------------ *)
+(* That it is me, tryint (and with succesfull I guess) creating a notation to [forall] *)
+Notation "∀ n , P" := (forall n, P) (at level 200, n ident).
+Theorem plus_O_n_test_me : ∀ n, (0 + n = n).
+Proof.
+  intros m. reflexivity. Qed.
+(* ------------------------------------------------------------------------ *)
+
+
+(** The keywords [intros], [simpl], and [reflexivity] are examples of
+    _tactics_.  A tactic is a command that is used between [Proof] and
+    [Qed] to guide the process of checking some claim we are making.
+    We will see several more tactics in the rest of this chapter and
+    many more in future chapters. *)
+
+(** Other similar theorems can be proved with the same pattern. *)
+
+Theorem plus_1_l : ∀ n, (1 + n = S n).
+Proof.
+  intros n. reflexivity. Qed.
+Theorem mult_0_l : ∀ n, (0 * n = 0).
+Proof.
+  intros n. reflexivity. Qed.
+
+(* The [_l] suffix in the names of these theorems is pronounced "on the left." *)
+
+
+
+(* ------------------------------------------------------------------------ *)
+(* ------------------------------------------------------------------------ *)
+(* Proof by Rewriting *)
+
