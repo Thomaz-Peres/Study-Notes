9b84a64aa8a7c69e4db626a934727ce88d1e3680
Thomaz Peres <thomaz.peres21@hotmail.com>
2023-12-16 12:41:31 -0300

    Starting doubly linked list

--- Data_Structures/WithCsharp/DataStructure/LinkedList/Singly/SinglyList.cs
+++ Data_Structures/WithCsharp/DataStructure/LinkedList/Singly/SinglyList.cs
@@ -1,132 +1,128 @@
-namespace DataStructure.LinkedList.Singly
-{
-    public record SinglyListNode<T>
-    {
-        public SinglyListNode(T data)
-        {
-            Data = data;
-            Next = null;
-        }
+namespace DataStructure.LinkedList.Singly;
 
-        public T Data { get; set; }
-        public SinglyListNode<T>? Next { get; set; }
+public record SinglyListNode<T>
+{
+    public SinglyListNode(T data) =>
+        Data = data;
+
+    public T Data { get; set; }
+    public SinglyListNode<T>? Next { get; set; }
+}
+
+public class SinglyList<T>
+{
+    private SinglyListNode<T>? Head { get; set; }
+
+    public SinglyListNode<T> StartList(T data)
+    {
+        var newList = new SinglyListNode<T>(data);
+        return Head = newList;
     }
 
-    public class SinglyList<T>
+    public SinglyListNode<T> AddLast(T data)
     {
-        private SinglyListNode<T>? Head { get; set; }
-
-        public SinglyListNode<T> StartList(T data)
+        SinglyListNode<T> newElement = new SinglyListNode<T>(data);
+        if (Head == null)
         {
-            var newList = new SinglyListNode<T>(data);
-            return Head = newList;
-        }
-
-        public SinglyListNode<T> AddLast(T data)
-        {
-            SinglyListNode<T> newElement = new SinglyListNode<T>(data);
-            if (Head == null)
-            {
-                Head = newElement;
-                return newElement;
-            }
-
-            var tempElement = Head;
-
-            while (tempElement.Next != null)
-            {
-                tempElement = tempElement.Next;
-            }
-
-            tempElement.Next = newElement;
+            Head = newElement;
             return newElement;
         }
 
-        public T GetIndex(int index)
+        var tempElement = Head;
+
+        while (tempElement.Next != null)
         {
-            if (Head == null)
-                return default;
-
-            var tempElement = Head;
-
-            if (index < 0)
-                throw new ArgumentOutOfRangeException(nameof(index));
-
-            for (int i = 0; tempElement != null && i < index; i++)
-            {
-                tempElement = tempElement.Next;
-            }
-
-            if (tempElement == null)
-                throw new ArgumentOutOfRangeException(nameof(index));
-
-            return tempElement.Data;
+            tempElement = tempElement.Next;
         }
 
-        public int GetLenght()
+        tempElement.Next = newElement;
+        return newElement;
+    }
+
+    public T? GetIndex(int index)
+    {
+        if (Head == null)
+            return default;
+
+        var tempElement = Head;
+
+        if (index < 0)
+            throw new ArgumentOutOfRangeException(nameof(index));
+
+        for (int i = 0; tempElement != null && i < index; i++)
         {
-            if (Head == null)
-                return 0;
-
-            var tempElement = Head;
-
-            int lenght = 1;
-            while (tempElement != null)
-            {
-                tempElement = tempElement.Next;
-                lenght++;
-            }
-
-            return lenght;
+            tempElement = tempElement.Next;
         }
 
-        public List<T> GetListData()
+        if (tempElement == null)
+            throw new ArgumentOutOfRangeException(nameof(index));
+
+        return tempElement.Data;
+    }
+
+    public int GetLenght()
+    {
+        if (Head == null)
+            return 0;
+
+        var tempElement = Head;
+
+        int lenght = 1;
+        while (tempElement != null)
         {
-            if (Head == null)
-                return new List<T>();
-
-            var tempElement = Head;
-            List<T> list = new();
-
-            while (tempElement != null)
-            {
-                list.Add(tempElement.Data);
-                tempElement = tempElement.Next;
-            }
-
-            return list;
+            tempElement = tempElement.Next;
+            lenght++;
         }
 
-        public bool DeleteElement(T delete)
+        return lenght;
+    }
+
+    public List<T> GetListData()
+    {
+        if (Head == null)
+            return new List<T>();
+
+        var tempElement = Head;
+        List<T> list = new();
+
+        while (tempElement != null)
         {
-            if (Head == null)
-                return false;
+            list.Add(tempElement.Data);
+            tempElement = tempElement.Next;
+        }
 
-            var currentElement = Head;
-            var tempElement = Head;
+        return list;
+    }
 
-            while (currentElement != null)
+    public bool DeleteElement(T delete)
+    {
+        if (Head == null)
+            return false;
+
+        var currentElement = Head;
+        var tempElement = Head;
+
+        while (currentElement != null)
+        {
+            if (currentElement.Data != null && currentElement.Data.Equals(delete))
             {
-                if (currentElement.Data != null && currentElement.Data.Equals(delete))
+                if (currentElement.Equals(Head))
                 {
-                    if (currentElement.Equals(Head))
-                    {
-                        Head = Head.Next;
-                        return true;
-                    }
-                    
-                    if (tempElement != null)
-                    {
-                        tempElement.Next = currentElement.Next;
-                        return true;
-                    }
+                    Head = Head.Next;
+                    return true;
                 }
 
-                tempElement = currentElement;
-                currentElement = currentElement.Next;
+                if (tempElement != null)
+                {
+                    tempElement.Next = currentElement.Next;
+                    return true;
+                }
             }
 
-            return false;
+            tempElement = currentElement;
+            currentElement = currentElement.Next;
         }
+
+        return false;
     }
 }
