0cec1e12a3d9c031799d5e07be38e6122687db61
Thomaz Peres <thomaz.peres21@hotmail.com>
2023-12-31 19:33:24 -0300

    Rust rust rust rustrust

---
+++ Languages/Rust/some_tests_to_learn/src/main.rs
@@ -0,0 +1,42 @@
+// // use std::cmp::PartialOrd;
+
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let result = largest(&number_list);
+//     println!("The largest number is {}", result);
+
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+
+//     let result = largest(&char_list);
+//     println!("The largest char is {}", result);
+// }
+
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+fn longest<'c>(x: &'c str, y: &'c str) -> &'c str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
