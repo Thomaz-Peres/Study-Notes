6103ac1ac2cb1355ff383f33d2e92c6c30826206
Thomaz Peres <thomaz.peres21@hotmail.com>
2024-04-14 21:17:14 -0300

    finishing

--- ProblemSolving/Rust/p1930/src/main.rs
+++ ProblemSolving/Rust/p1930/src/main.rs
@@ -2,14 +2,11 @@ use std::io::Error;
 
 fn main() -> std::io::Result<()> {
     let mut line = String::new();
-    std::io::stdin().read_line(&mut line).expect("Failed to readline");
+    std::io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to readline");
 
-    // if line.replace(" ", "").len() > 4
-    // {
-    //     return Err(Error::new(std::io::ErrorKind::InvalidData, "Input too long"));
-    // }
-
-    let mut v : Vec<i32> = Vec::new();
+    let mut v: Vec<i32> = Vec::new();
 
     let mut x = line.split_whitespace();
     v.push(x.next().unwrap().parse().unwrap());
@@ -17,7 +14,25 @@ fn main() -> std::io::Result<()> {
     v.push(x.next().unwrap().parse().unwrap());
     v.push(x.next().unwrap().parse().unwrap());
 
+    if v.len() != 4 {
+        return Err(Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Input too long",
+        ));
+    }
 
-    print!("{:?}", v);
+    let mut plus: i32 = 0;
+    for i in &v {
+        if *i < 2 || *i > 6 {
+            return Err(Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Input too long",
+            ));
+        }
+
+        plus += i;
+    }
+
+    print!("{}\n", plus - 3);
     Ok(())
 }
