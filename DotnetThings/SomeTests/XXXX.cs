27c9ef1544c5f16da9ce0938ae182dd0eae67153
Thomaz Peres <thomaz.peres21@hotmail.com>
2024-05-22 21:25:31 -0300

    *

---
+++ DotnetThings/SomeTests/XXXX.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+public class Marmore
+{
+  public static void Main(string[] args)
+  {
+    var x = new teste.Teste20();
+
+    x.Load(default);
+    // Console.WriteLine(x);
+
+    //object teste = default;
+
+    //if (teste is null)
+    //	throw new ArgumentNullException($"{nameof(teste)} {new StackTrace(new StackFrame().GetFileColumnNumber())}");
+  }
+}
