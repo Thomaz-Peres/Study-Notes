4577bec30ce51eab53123ac04a9ba5b8496e6e20
Thomaz Peres <thomaz.peres21@hotmail.com>
2024-05-25 18:58:29 -0300

    *

--- Data_Structures/typescript/binarytree.ts
+++ Data_Structures/typescript/binarytree.ts
@@ -3,11 +3,21 @@ type List<T> = null | {
     next: List<T>
 }
 let empty = null;
-
+const isEmpty = <A>(list: List<A>): list is null => list === null;
 const append = <T>(list: List<T>, element: T) : List<T> => ({
     element: element,
     next: list
 });
+
+const map = <A,B>(f: (el: A) => B, l: List<A>): List<B> => {
+    if (isEmpty(l))
+        return null;
+    else {
+        const elemment = f(l.element);
+        const next = map(f, l.next);
+        return append(next, elemment)
+    }
+}
 
 const list_one: List<string> = append(empty, "a");
 const list_two = { element: 2, next: list_one };
