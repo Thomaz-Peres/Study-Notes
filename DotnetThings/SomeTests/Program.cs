2aa59a31a54824782aa4e2f327f11aec6767cee4
Thomaz Peres <thomaz.peres21@hotmail.com>
2024-08-02 15:35:46 -0300

    New

--- DotnetThings/SomeTests/Program.cs
+++ DotnetThings/SomeTests/Program.cs
@@ -7,6 +7,8 @@ Console.WriteLine(z.MyPropertyobject);
 Console.WriteLine(z.MyPropertystring);
 Console.WriteLine(z.MyPropertygenericWork);
 // Console.WriteLine(z.MyProperty2);
-Console.WriteLine(Bitwise.TesteBitwise2.Name2 & Bitwise.TesteBitwise2.Name);
-Console.WriteLine(Bitwise.TesteBitwise2.Name2 | Bitwise.TesteBitwise2.Name);
+Console.WriteLine(Bitwise.TesteBitwise2.Name2 & Bitwise.TesteBitwise2.Name); // print small number
+Console.WriteLine(Bitwise.TesteBitwise2.Name2 | Bitwise.TesteBitwise2.Name); // print higher number
+Console.WriteLine(Bitwise.TesteBitwise2.Name2 | Bitwise.TesteBitwise2.Name | Bitwise.TesteBitwise2.Name3); // print the sum, and the sum is Name4, nice
+Console.WriteLine(Bitwise.TesteBitwise2.Name2 | Bitwise.TesteBitwise2.Name | Bitwise.TesteBitwise2.Name3 | Bitwise.TesteBitwise2.Name4); // print the higher
 Console.WriteLine(~Bitwise.TesteBitwise2.Name2);
