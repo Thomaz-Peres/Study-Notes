777ca663c9403af998b3cca423d3ce40b7cfb6ea
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-01-11 01:38:50 -0300

    *

--- ProblemSolving/CSharp/newyearchaos/Class1.cs
+++ ProblemSolving/CSharp/newyearchaos/Class1.cs
@@ -1,18 +1,4 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
-class Result
+﻿class Result
 {
 
     /*
@@ -24,6 +10,19 @@ class Result
     public static void minimumBribes(List<int> q)
     {
         int bribes = 0;
+
+        for (int i = q.Count - 1; i >= 0; i--) {
+            if (q[i] > q[i] - 1)
+                bribes++;
+
+            if (q[i] > q[i] - 2)
+                bribes++;
+
+            if (bribes >= 3) {
+                Console.WriteLine("Too chaotic");
+                return;
+            }
+        }
 
     }
 
