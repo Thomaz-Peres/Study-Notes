276597a24b4d2f69ad0de40154ff1a1c188851bb
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-01-17 22:21:44 -0300

    Finished, trying to make O(n)

---
+++ ProblemSolving/CSharp/leetcode/Leetcode.cs
@@ -0,0 +1,18 @@
+﻿#nullable disable
+namespace leetcode;
+
+public class Leetcode
+{
+    static void Main(string[] args)
+    {
+        int[] nums = Array.ConvertAll(Console.ReadLine().Split(','), arr => Convert.ToInt32(arr));
+        var target = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(string.Join(",", new Solution().TwoSum(nums, target)));
+    }
+}
+
+
+
+
+
+#nullable enable
