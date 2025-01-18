a42868679496cc44d648edf804c75cfde35c0382
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-01-17 23:35:50 -0300

    Done

--- ProblemSolving/CSharp/leetcode/twosum.cs
+++ ProblemSolving/CSharp/leetcode/twosum.cs
@@ -2,11 +2,13 @@ namespace leetcode;
 
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
+        var map = new Dictionary<int, int>();
         for (int i = 0; i < nums.Length; i++) {
-            for (int j = i + 1; j < nums.Length; j++) {
-                if (nums[i] + nums[j] == target)
-                    return [i, j];
-            }
+            var x = target - nums[i];
+            if (map.ContainsKey(x))
+                return new int[] { map[x], i };
+
+            map[nums[i]] = i;
         }
 
         return Array.Empty<int>();
