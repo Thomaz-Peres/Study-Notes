09b30b45ff1f17d94808ea69392c59a90339253a
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-01-21 19:19:57 -0300

    Finished binary

--- Data_Structures/Csharp/DataStructure/Algorithms/BinarySearch.cs
+++ Data_Structures/Csharp/DataStructure/Algorithms/BinarySearch.cs
@@ -6,16 +6,19 @@ public static class BinarySearch
     {
         Array.Sort(ar);
         int i = 0;
+        int r = ar.Length - 1;
         while (i <= ar.Length - 1)
         {
-            int x = (ar.Length - i) / 2;
+            int x = (r + i) / 2;
             if (t > ar[x]) {
                 i = x + 1;
             } else if (t < ar[x]) {
-                i = x - 1;
+                r = x - 1;
+            } else {
+                return ar[x];
             }
         }
 
-        return i;
+        return default;
     }
 }
--- Data_Structures/Csharp/Tests/AlgorithmsTests/BinarySearchTests.cs
+++ Data_Structures/Csharp/Tests/AlgorithmsTests/BinarySearchTests.cs
@@ -1,13 +1,14 @@
-using bs = DataStructure.Algorithms.BinarySearch;
+using Bs = DataStructure.Algorithms.BinarySearch;
 
 namespace Tests.AlgorithmsTests;
 
 public static class BinarySearchTests
 {
     [Theory]
-    [InlineData(new int[] {5, 8, 7, 9, 6, 3, 10, 25, 2}, 3)]
+    [InlineData(new int[] { 5, 8, 7, 9, 6, 3, 10, 25, 2 }, 3)]
+    [InlineData(new int[] { 5, 8, 7, 3, 2, 10, 9, 1, 4, 6 }, 9)]
     public static void IntLinkedList(int[] arr, int t)
     {
-        Assert.Equal(t, bs.SearchOption(arr, t));
+        Assert.Equal(t, Bs.SearchOption(arr, t));
     }
 }
