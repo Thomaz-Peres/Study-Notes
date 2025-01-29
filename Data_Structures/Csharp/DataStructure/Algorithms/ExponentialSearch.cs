74c37998703cbda2d9e13e24011d935fcb8b6ac0
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-01-28 23:11:52 -0300

    Finishing Exponential Search

--- Data_Structures/Csharp/DataStructure/Algorithms/ExponentialSearch.cs
+++ Data_Structures/Csharp/DataStructure/Algorithms/ExponentialSearch.cs
@@ -2,16 +2,29 @@ namespace DataStructure.Algorithms;
 
 public static class ExponentialSearch
 {
-    public static int Es(int[] ar, int key, int size)
+    public static int Es(int[] ar, int key)
     {
-        if (size == 0)
+        if (ar.Length - 1 == 0)
+            return -1;
+
+        // Ensure the array is sorted (ideally, this should be done beforehand)
+        Array.Sort(ar);
+
+        // If the key is at the first position
+        if (ar[0] == key)
             return 0;
 
         int bound = 1;
-        while (bound < size && ar[bound] < key) {
+        while (bound < ar.Length - 1 && ar[bound] < key) {
             bound *= 2;
         }
 
-        return Array.BinarySearch(ar, key, bound/2, Math.Min(bound, size));
+        int start = bound / 2;
+        int end = Math.Min(bound, ar.Length - 1);
+
+        int[] subArray = new int[end - start + 1];
+        Array.Copy(ar, start, subArray, 0, subArray.Length);
+
+        return BinarySearch.SearchOption(subArray, key);
     }
 }
--- Data_Structures/Csharp/Tests/AlgorithmsTests/BinarySearchTests.cs
+++ Data_Structures/Csharp/Tests/AlgorithmsTests/BinarySearchTests.cs
@@ -1,4 +1,5 @@
 using Bs = DataStructure.Algorithms.BinarySearch;
+using Es = DataStructure.Algorithms.ExponentialSearch;
 
 namespace Tests.AlgorithmsTests;
 
@@ -10,5 +11,13 @@ public static class BinarySearchTests
     public static void IntLinkedList(int[] arr, int t)
     {
         Assert.Equal(t, Bs.SearchOption(arr, t));
+    }
+
+    [Theory]
+    [InlineData(new int[] { 5, 8, 7, 9, 6, 3, 10, 25, 2 }, 3)]
+    [InlineData(new int[] { 5, 8, 7, 3, 2, 10, 9, 1, 4, 6 }, 9)]
+    public static void ExponentialSearch(int[] arr, int key)
+    {
+        Assert.Equal(key, Es.Es(arr, key));
     }
 }
