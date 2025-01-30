b05fc5c94a4904d2a1238c9d11e11d52c82d163d
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-01-29 23:56:46 -0300

    *

---
+++ ProblemSolving/CSharp/leetcode/_206.cs
@@ -0,0 +1,17 @@
+namespace leetcode;
+
+
+public class ListNode {
+    public int val;
+    public ListNode? next;
+    public ListNode(int val = 0, ListNode? next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public static class Solution206 {
+    public static ListNode ReverseListRecursive(ListNode head) {
+
+    }
+}
