7187fe1959aae86b817335185302875e4c50feec
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-01-30 23:05:05 -0300

    reverse list

---
+++ ProblemSolving/CSharp/Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using leetcode;
+
+namespace tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ReverseListRecursive()
+    {
+        var x = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+        var b = Solution206.ReverseListRecursive(x);
+
+        Assert.Equal(new ListNode(5, new ListNode(4, new ListNode(3, new ListNode(2, new ListNode(1))))), b);
+    }
+}
--- ProblemSolving/CSharp/leetcode/_206.cs
+++ ProblemSolving/CSharp/leetcode/_206.cs
@@ -1,17 +1,28 @@
 namespace leetcode;
 
-
-public class ListNode {
+public class ListNode
+{
     public int val;
     public ListNode? next;
-    public ListNode(int val = 0, ListNode? next = null) {
+    public ListNode(int val = 0, ListNode? next = null)
+    {
         this.val = val;
         this.next = next;
     }
 }
 
-public static class Solution206 {
-    public static ListNode ReverseListRecursive(ListNode head) {
+public static class Solution206
+{
+    public static ListNode ReverseListRecursive(ListNode head)
+    {
+        if (head == null)
+            return new ListNode();
 
+        if (head.next == null)
+            return head;
+
+
+        head.next = ReverseListRecursive(head.next);
+        return head;
     }
 }
