620476734fdf0ed99ec8a383c0ed6cc7ebcd973f
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-02-24 23:43:51 -0300

    Let's start new one

---
+++ ProblemSolving/CSharp/leetcode/Leetcod/_121.cs
@@ -0,0 +1,16 @@
+namespace leetcode;
+
+public static class Solution121
+{
+    public static int MaxProfit(int[] prices) {
+        var p = 0;
+        var d = new HashSet<int>();
+
+        for (var i = 0; i <= prices.Length - 1; i++)
+        {
+
+        }
+
+        return p;
+    }
+}
