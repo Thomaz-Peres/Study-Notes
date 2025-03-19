5a9ad7a211a977b13464db951fb63da91ac34ae1
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-03-18 23:23:44 -0300

    After Some days, I back and finished

--- ProblemSolving/CSharp/leetcode/Leetcod/_733.cs
+++ ProblemSolving/CSharp/leetcode/Leetcod/_733.cs
@@ -1,12 +1,28 @@
-public static class Solution733 {
+public class Solution733 {
     public static int[][] FloodFill(int[][] image, int sr, int sc, int color) {
-        if (sr < 0 || sc < 0 && sr > image.Length)
-            return image;
-
-        image[sr][sc] = color;
-        FloodFill(image, sr - 1, sc, color);
-        FloodFill(image, sr + 1, sc, color);
+        var current = image[sr][sc];
+        if (current != color)
+            Fill(image, sr, sc, current, color);
 
         return image;
+    }
+
+    private static void Fill(int[][] image, int sr, int sc, int oldColo, int newColor)
+    {
+        if (image[sr][sc] == oldColo)
+        {
+            image[sr][sc] = newColor;
+            if (sr >= 1)
+                Fill(image, sr - 1, sc, oldColo, newColor);
+
+            if (sc >= 1)
+                Fill(image, sr, sc - 1, oldColo, newColor);
+
+            if (sr + 1 < image.Length)
+                Fill(image, sr + 1, sc, oldColo, newColor);
+
+            if (sc + 1 < image[0].Length)
+                Fill(image, sr, sc + 1, oldColo, newColor);
+        }
     }
 }
--- ProblemSolving/CSharp/leetcode/Tests/Helpers.cs
+++ ProblemSolving/CSharp/leetcode/Tests/Helpers.cs
@@ -58,4 +58,14 @@ public partial class UnitTest1
                 -1
             },
         };
+
+    public static IEnumerable<object[]> FloodFill =>
+        new List<object[]>
+        {
+            new object[] {
+                new int[] [[1,1,1],[1,1,0],[1,0,1]],
+                2,
+                -1
+            },
+        };
 }
